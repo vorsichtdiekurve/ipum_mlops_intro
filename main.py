1c3e2f205e13315244206b29d96edb807b7f18d2
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-04-18 14:36:15 +0200

    WiP

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from ipum-mlops-intro!")
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    load_dotenv(f".env.{environment}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+from pydantic_settings import BaseSettings
+from pydantic import validator
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @validator("environment")
+    def validate_environment(cls, value):
+        if value not in ["dev", "test", "prod"]:
+            raise ValueError("ENVIRONMENT must be one of (dev, test, prod)")
+        return value
